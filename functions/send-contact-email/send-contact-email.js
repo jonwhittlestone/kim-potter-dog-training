f194cc8270a0ec38be9b001ad0678503ab5cca10
Jon Whittlestone <dev@howapped.com>
2020-07-08 20:55:58 +0100

    emailer validation

--- functions/send-contact-email/send-contact-email.js
+++ functions/send-contact-email/send-contact-email.js
@@ -33,7 +33,7 @@ exports.handler = async (event) => {
   const data = JSON.parse(event.body)
   if (
       !data.message || !data.contactName &&
-      (!data.contactEmail && !data.contactPhone)
+      (data.contactEmail != '' && data.contactPhone != '')
     ) {
     return { statusCode: 422, body: 'Name, message and phone or email are required.' }
   }
